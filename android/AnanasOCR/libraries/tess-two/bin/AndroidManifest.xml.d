329cae6a1e78629c22f6064d4b9e126591c3978d
wqhuang-ustc <wqhuang.ustc@gmail.com>
2017-01-13 19:37:04 +0000

    delete unrelated folder

--- android/AnanasOCR/libraries/tess-two/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/andeye/Skola/mobileCloudComputing/mcc-2016-g02-p2/android/tess-two/tess-two/bin/AndroidManifest.xml : \
-/home/andeye/Skola/mobileCloudComputing/mcc-2016-g02-p2/android/tess-two/tess-two/AndroidManifest.xml \
